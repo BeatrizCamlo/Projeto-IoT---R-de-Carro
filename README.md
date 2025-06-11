1197b48a775decdf46d53f0a65c0404e6aef7dff
beatriz-camilo <abeatrizcamilo@gmail.com>
2025-06-11 14:34:53 -0300

    Adicionar arquivos de documentação

---
+++ README.md
@@ -0,0 +1,30 @@
+***Projeto Ré Automotivo***
+
+**UNIDADE I**  
+
+
+O sistema de ré de um carro é responsável por identificar objetos atrás do carro e sinalizar ao montorista o qunato mais ele pode se aproximar sem causar danos ao carro e similarmente aos objetos próximos.
+
+O projeto é composto por:
+- 1 Arduino Uno;
+- 3 Leds (Verde, Amarelo, Vermelho);
+- 1 Sensor Ultrassônico;
+- 1 Buzzer
+- 1 Display LCD 16x2.
+
+**Funcionamento do Projeto**  
+
+O sensor ultrassonico é responsável por identifiar a distância e definir o comportamento dos leds. Se a distância identificada for maior que 300cm, o led verde pisca de forma lenta e é exibido no display a mensagem "Distância segura.". Caso a distância seja maior 50 a luz amarela pisca com maior frequência e é exibido no diplay a mensagem "Distância arriscada. Atenção!". Já para distânica menores que 50, o led vermelho fica acessa, o buzzer dispara e é exibida a mensagem no display "Distância intensa. Pare!".
+
+**UNIDADE II**  
+
+
+Para complementar as funcionalidades do Sensor de Ré Automotivo, serão adicionados:
+
+- 1 ESP32;
+- Sensor de Volume;
+- Integração com API GPS;
+- Integração com API de informações sobre o tempo;
+- Dashboard em Adafruit.
+
+As novas extensões tem como próposito fornecer informações sobre a porcentagem de gasolina presente no tanque com o sensor de volume, consumo de API GPS e apresentação no display LCD para o cliente, consumo de API para informações do tempo que serão também apresentadas, dashboard dos dados dos sensores na plataforma Adafruit e modelo de rastreamento para o carro do cliente.
---
+++ main.c
@@ -0,0 +1,76 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h> 
+#define trigPin 12
+#define echoPin 11
+#define ledVermelho 7
+#define ledAmarelo 6
+#define ledVerde 5
+#define buzzer 13
+#define lin 2
+#define col 16
+#define index 0x27
+
+LiquidCrystal_I2C display(index,col,lin);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  display.init();
+  display.backlight();
+  display.clear();
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long tempo = pulseIn(echoPin, HIGH);
+  float distancia = (tempo * 0.0343) / 2;
+
+  display.setCursor(0,0); 
+  display.print("PROJETO IOT"); 
+  display.setCursor(0, 1); 
+  delay(2000);  
+  display.clear(); 
+  delay(500);
+
+  if(distancia > 100){
+    digitalWrite(ledVerde, HIGH);
+    display.setCursor(0,0); 
+    display.print("DISTANCIA SEGURA.");
+    delay(2000);
+    display.clear();
+    digitalWrite(ledVerde, LOW);  
+    }
+
+    else if (distancia > 50){
+      digitalWrite(ledAmarelo, HIGH);
+      display.setCursor(0,0); 
+      display.print("DISTANCIA ARRISCADA."); 
+      display.setCursor(0, 1); 
+      display.print("ATENCAO!");
+      delay(2000);
+      display.clear();
+      digitalWrite(ledAmarelo, LOW); 
+
+      } else{
+          digitalWrite(buzzer, HIGH);
+          digitalWrite(ledVermelho, HIGH);
+          display.setCursor(0,0); 
+          display.print("PARE!");
+          delay(2000);a
+          display.clear();
+          digitalWrite(ledVermelho, LOW);
+          digitalWrite(buzzer, LOW);  
+        }
+
+  Serial.print("Distância: ");
+  Serial.print(distancia);
+  Serial.print(" cm\n");
+
+  delay(1000);
+}

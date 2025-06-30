2e37aa6c6886e88df2fb5e1e2bdc27393498fb50
Beatriz Camilo <abeatrizcamilo@gmail.com>
2025-06-30 15:38:20 -0300

    Adicionando arquivos necessários para a entrada de imagens via HTTP e RTSP e processadas por servidor web Flask e biblioteca EasyOCR. Ocultando informações críticas

--- src/sistema-re-automotivo/main.py
+++ src/sistema-re-automotivo/main.py
@@ -5,15 +5,13 @@ import ujson
 from umqtt.simple import MQTTClient
 import lcd_api
 import i2c_lcd
-from config import ADAFRUIT_AIO_USERNAME, ADAFRUIT_AIO_KEY, ADAFRUIT_AIO_URL
+from config.py import ADAFRUIT_AIO_USERNAME, ADAFRUIT_AIO_KEY, ADAFRUIT_AIO_URL, FEED_NAME
 
 # ==== Configurações Wi-Fi ====
 SSID = "Wokwi-GUEST"
 PASSWORD = ""
 
 # ==== Configurações MQTT (Adafruit IO) ====
-
-FEED = ADAFRUIT_AIO_USERNAME + "/feeds/distancia_sensor_re"
 
 # ==== Configuração de pinos ====
 

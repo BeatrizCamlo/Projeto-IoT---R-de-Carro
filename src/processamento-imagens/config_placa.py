f7281ef671ae86843390cc2f2bc4a82055df24ab
Beatriz Camilo <abeatrizcamilo@gmail.com>
2025-06-30 15:49:55 -0300

    Adicionando arquivos necessários para a entrada de imagens via HTTP e RTSP e processadas por servidor web Flask e biblioteca EasyOCR. Ocultando informações críticas

---
+++ src/processamento-imagens/config_placa.py
@@ -0,0 +1,2 @@
+ADAFRUIT_AIO_USERNAME = "BeatrizCamilo"
+ADAFRUIT_AIO_KEY      = "aio_Tqvm06cbudGTJ9OqX57mliVtFrtP"
---
+++ src/sistema-re-automotivo/config_sensor.py
@@ -0,0 +1,6 @@
+# config.py
+
+ADAFRUIT_IO_URL = "io.adafruit.com"
+ADAFRUIT_USERNAME = "LunaKarolix"
+ADAFRUIT_IO_KEY = "aio..."
+FEED_NAME = ADAFRUIT_AIO_USERNAME + "/feeds/distancia_sensor_re"
